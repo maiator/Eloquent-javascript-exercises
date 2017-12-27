885ea30c580c3e6b5802e9e7c894f47c07733b55
Rodney Maiato <maiator@users.noreply.github.com>
2017-12-26 20:57:41 -0500

    Added the official answer from the author

--- exercise_2-3.js
+++ exercise_2-3.js
@@ -9,3 +9,8 @@ for (i = 0; i < 7; i++) {
     it creates ####### on the seventh line.
     */
 }
+/* The official answer from that the offer gives is much better code in the sense that it utilizes string.length method to create
+the following code */
+
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
