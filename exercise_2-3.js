b5d444cda27cab972b7702e9fc20007906f56804
Rodney Maiato <maiator@users.noreply.github.com>
2017-12-26 19:18:20 -0500

    Update exercise_2-3.js

--- exercise_2-3.js
+++ exercise_2-3.js
@@ -1,4 +1,11 @@
 var hTag = "";
 for (i = 0; i < 7; i++) {
-    console.log(hTag += "#"); // this will print hTag
+    console.log(hTag += "#"); 
+    
+    /* 
+    this will print hTag 7 times. 
+    The first time it will print null character + the # to produce #.
+    Then it will go to a new line to produce ## and so on until
+    it creates ####### on the seventh line.
+    */
 }
